d905ba67ee02b116790b6bd47fca79d0e7fda0c4
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-08-07 19:35:53 +0300

    Added tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe Question, type: :model do
   it 'have many attached files' do
     expect(create(:question).files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
+
+  describe 'voting' do
+    let!(:question) { create :question }
+    let!(:vote) { create :vote, votable: question, rate: 1 }
+    let(:user) { create :user }
+
+    it 'rating' do
+      expect(question.rating).to eq 1
+    end
+
+    it 'can\'t vote own' do
+      expect { question.vote(question.author, 1) }.to_not change { question.rating }
+    end
+
+    it 'votes +' do
+      expect { question.vote(user, 1) }.to change { question.rating }.by 1
+    end
+
+    it 'votes -' do
+      expect { question.vote(user, -1) }.to change { question.rating }.by(-1)
+    end
+
+    it 'unvotes' do
+      question.vote(user, 1)
+      expect { question.unvote(user) }.to change { question.rating }.by(-1)
+    end
+    end
+
 end
