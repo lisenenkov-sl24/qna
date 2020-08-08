3d17771a7d884b87fcf3a1526747810448b1bb78
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-08-08 12:42:42 +0300

    Changed validate syntax

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
   belongs_to :user
 
-  validates_uniqueness_of :user, scope: :votable, message: 'already voted'
-  validates_inclusion_of :rate, in: [-1, 1]
+  validates :user, uniqueness: { scope: :votable, message: 'already voted' }
+  validates :rate, inclusion: { in: [-1, 1] }
 end
